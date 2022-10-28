7f9352ad31472511d53b224dcf1909be93248044
TrungNguyen <63598647+TrungNguyen0204@users.noreply.github.com>
2022-10-28 21:18:30 +0700

    Add program

---
+++ KmeanPic/Kmean.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmeanPic
+{
+    class Kmean
+    {
+        //Cluster Quantity
+        static int ClusterQuantity = 3;
+        
+        //Kmean Algorithm
+        public static List<Cluster> kmean(List<Picture> PictureList)
+        {
+            //Cluster List
+            List<Cluster> ClusterList = new List<Cluster>();
+            List<Cluster> newClusterList = new List<Cluster>();
+            //Choose the root vector for each cluster(1-10 in vector list)
+            for (int i = 0; i < ClusterQuantity; i++)
+            {
+                Cluster clt = new Cluster(PictureList[i].Vector);
+                ClusterList.Add(clt);
+            }
+            //Copy ClusterList to newClusterList
+            for (int i = 0; i < ClusterQuantity; i++)
+            {
+                Cluster clt = new Cluster(PictureList[i].Vector);
+                newClusterList.Add(clt);
+            }
+            while (true)
+            {
+                foreach (var Picture in PictureList)
+                {
+                    //Calculate the distance between Image's Vector and Cluster's Vector
+                    List<double> AllDistance = new List<double>();
+                    foreach (var item in newClusterList)
+                    {
+                        AllDistance.Add(VectorHandle.Distance(Picture.Vector, item.VectorOC));
+                    }
+                    //Clustering for Picture
+                    foreach (var item in newClusterList)
+                    {
+                        if (VectorHandle.Distance(Picture.Vector, item.VectorOC) == AllDistance.Min())
+                            item.PicturesListOC.Add(Picture);
+                    }
+                }
+                //Recalculate cluster's vector
+                foreach (var item in newClusterList)
+                {
+                    Vector newVector = new Vector();
+                    for (int i = 0; i < item.VectorOC.Properties.Count; i++)
+                    {
+                        double average = 0;
+                        foreach (var Picture in item.PicturesListOC)
+                        {
+                            average += Picture.Vector.Properties[i];
+                        }
+                        average = average / item.PicturesListOC.Count;
+                        newVector.Properties.Add(average);
+                    }
+                    item.VectorOC = newVector;
+                }
+                //Check to end of while
+                bool checkToEnd = true;
+                for (int i = 0; i < ClusterList.Count; i++)
+                {
+                    if (newClusterList[i] != ClusterList[i])
+                    {
+                        ClusterList = newClusterList;
+                        checkToEnd = false;
+                    }
+                }
+                if (checkToEnd)
+                    break;
+                //Clear Pictures List of all cluster
+                foreach (var item in newClusterList)
+                {
+                    item.PicturesListOC.Clear();
+                }
+            }
+            return ClusterList;
+        }
+    }
+}
---
+++ KmeanPic/VectorHandle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmeanPic
+{
+    class VectorHandle
+    {
+        //Read file 
+        public static void input(string fileName, List<Picture> listItem)
+        {
+            listItem.Clear();
+            var strs = File.ReadAllLines(fileName);
+            foreach (var str in strs)
+            {
+                Picture img = new Picture();
+                Vector vt = new Vector();
+                //Split line 
+                var vector = str.Trim().Split(' ', '(', ')');
+                img.Id = vector[0];
+                //Save feature vector
+                for (int i = 1; i < vector.Length - 2; i++)
+                {
+                    if (vector[i].Contains(".jpg"))
+                        img.ImgName = vector[i];
+                    else if (Double.TryParse(vector[i], out double v))
+                        vt.Properties.Add(v);
+                }
+                img.Vector = vt;
+                //Save Folder Name
+                img.FolderName = vector[vector.Length - 1];
+                listItem.Add(img);
+            }
+        }
+        //Distance function
+        public static double Distance(Vector a, Vector b)
+        {
+            double sum = 0;
+            for (int i = 0; i < a.Properties.Count; i++)
+                sum += Math.Pow((a.Properties[i] - b.Properties[i]),2.0);
+            return Math.Sqrt(sum);
+        }
+    }
+}
